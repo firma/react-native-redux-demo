f9b9add72802e935b86a3fb6379170baab236a8f
laurusCN <24383363@qq.com>
2016-11-04 20:53:40 +0800

    react native init

---
+++ index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {
+  AppRegistry,
+} from 'react-native';
+ 
+import App from './app/container/app';
+
+AppRegistry.registerComponent('lemonveil', () => App);
